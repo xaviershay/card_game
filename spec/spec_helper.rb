75ed7a0b728880ff92eb0235c717f98b1cbf4389
Xavier Shay <xavier@rhnh.net>
2015-08-07 19:23:13 -0700

    Aggregate failures evaluator for XSpec.

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,3 +1,30 @@
 require 'xspec'
 
-extend XSpec.dsl
+module AggregateFailures
+  include XSpec::Evaluator::Simple
+
+  def call(uow)
+    @failures = []
+
+    result = begin
+      super
+      []
+    rescue => e
+      [[e.message, e.backtrace]]
+    end
+
+    (@failures + result).map do |data|
+      XSpec::Failure.new(uow, *data)
+    end
+  end
+
+  def _raise(message)
+    @failures << [message, caller]
+  end
+end
+
+extend XSpec.dsl(
+  evaluator: XSpec::Evaluator.stack {
+    include AggregateFailures
+  }
+)
